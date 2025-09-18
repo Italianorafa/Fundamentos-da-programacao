837bbec1f3fbb6292265e22d84c25d669e758b15
Italianorafa <italianorafap@gmail.com>
2025-09-18 16:11:42 +0100

    aula 00

---
+++ aula00/area.py
@@ -0,0 +1,5 @@
+largura = float(input('Introduza a largura: '))
+altura = float(input('Introduza a altura: '))
+area = largura * altura
+perimetro = 2*(largura + altura)
+print('A area é ', area, ' e o perimetro é ', perimetro)
